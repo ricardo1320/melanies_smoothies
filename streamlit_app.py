1f5193a0f25893eb7a4cd0abf2ed3f72e41eae2f
Ricardo Cuevas Mondragon <ricardo.cuevas3105@gmail.com>
2025-12-30 15:57:05 +0100

    Fix ingredient string concatenation in order insertion

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        sf_df
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
